bf02da4575c4b5cca8940ddd681f7174e07d7b81
Anibal Torres <anibal_torres1991@hotmail.com>
2025-02-25 12:50:54 -0300

    Dia 1 del day of code

---
+++ app1.js
@@ -0,0 +1,37 @@
+//VARIABLES
+
+let numeroUn = 1;
+let stringUn = '1';
+let numeroTreinta = 30;
+let stringTreinta = '30';
+let numeroDiez = 10;
+let stringDiez = '10';
+
+//OPERACIONES
+
+console.log(typeof(numeroUn));
+console.log(typeof(stringUn));
+
+if(numeroUn === stringUn) {
+    console.log('Las variables numeroUn y stringUn tienen el mismo valor, pero tipos diferentes')
+} else {
+    console.log('Las variables numeroUn y stringUn no tienen el mismo valor')
+}
+
+console.log(typeof(numeroTreinta));
+console.log(typeof(stringTreinta));
+
+if(numeroTreinta === stringTreinta) {
+    console.log('Las variables numeroTreinta y stringTreinta tienen el mismo valor y el mismo tipo')
+} else {
+    console.log('Las variables numeroTreinta y stringTreinta no tienen el mismo valor')
+}
+
+console.log(typeof(numeroDiez));
+console.log(typeof(stringDiez));
+
+if(numeroDiez === stringDiez) {
+    console.log('Las variables numeroDiez y stringDiez tienen el mismo valor, pero tipos diferentes')
+} else {
+    console.log('Las variables numeroDiez y stringDiez no tienen el mismo valor')
+}
